b6b6175cf3cef6618280d40e3c067a5a694aa9b0
AaronYoung5 <aarony3131@gmail.com>
2022-01-12 15:16:46 -0600

    Added .github folder

--- setup.py
+++ setup.py
@@ -16,23 +16,40 @@ def parse_requirements():
 
     return required
 
-# Arguments marked as "Required" below must be included for upload to PyPI.
-# Fields marked as "Optional" may be commented out.
+def create_version():
+    from setuptools_scm.version import get_local_dirty_tag
 
+    def clean_scheme(version):
+        return get_local_dirty_tag(version) if version.dirty else '+clean'
+
+    return {'local_scheme': clean_scheme}
 
 setup(
     name='miniav',  # Required
-    version='0.0.0',  # Required
+    use_scm_version=create_version,
     description='MiniAV Project',  # Optional
     long_description=long_description,  # Optional
     long_description_content_type='text/markdown',  # Optional (see note above)
     author='UW Simulation Based Engineering Lab',  # Optional
+    license="BSD3",
     packages=find_packages(),  # Required
     python_requires='>=3.6, <4',
     install_requires=parse_requirements(),  # Optional
     entry_points={  # Optional
         'console_scripts': [
-            'miniav=miniav.miniav_base:main',
+            'miniav=miniav._miniav_base:main',
         ],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Software Development",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+    project_urls={  # Optional
+        "Homepage": "https://uwsbel.github.io/miniav",
+        "Bug Reports": "https://github.com/uwsbel/miniav/issues",
+        "Source Code": "https://github.com/uwsbel/miniav/",
+        "Our Lab!": "https://sbel.wisc.edu",
     },
 )
