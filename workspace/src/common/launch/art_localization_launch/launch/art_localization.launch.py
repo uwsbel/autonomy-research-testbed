45aa8050436651b4187e9e532e5d97c957790cf8
AaronYoung5 <aryoung@mit.edu>
2023-09-24 14:52:07 -0400

    Fixed small bugs regarding the localization config files

--- workspace/src/common/launch/art_localization_launch/launch/art_localization.launch.py
+++ workspace/src/common/launch/art_localization_launch/launch/art_localization.launch.py
@@ -50,7 +50,7 @@ def generate_launch_description():
     AddLaunchArgument(ld, "art_localization/input/accelerometer", "/sensing/accelerometer/data")
     AddLaunchArgument(ld, "art_localization/input/vehicle_inputs", "/control/vehicle_inputs")
     AddLaunchArgument(ld, "art_localization/input/ground_truth", "/vehicle/state")
-    AddLaunchArgument(ld, "art_localization/output/vehicle/filtered_state", "/vehicle/filtered_state")
+    AddLaunchArgument(ld, "art_localization/output/filtered_state", "/vehicle/filtered_state")
 
     AddLaunchArgument(ld, "estimation_alg", "ground_truth", choices=("ground_truth", "extended_kalman_filter", "particle_filter"))
 
@@ -69,7 +69,7 @@ def generate_launch_description():
                 ("~/input/accelerometer", GetLaunchArgument("art_localization/input/accelerometer")),
                 ("~/input/vehicle_inputs", GetLaunchArgument("art_localization/input/vehicle_inputs")),
                 ("~/input/ground_truth", GetLaunchArgument("art_localization/input/ground_truth")),
-                ("~/output/vehicle/filtered_state", GetLaunchArgument("art_localization/output/vehicle/filtered_state")),
+                ("~/output/filtered_state", GetLaunchArgument("art_localization/output/filtered_state")),
         ],
         parameters=[GetPackageSharePath("art_localization_launch", "config", "4DOF_dynamics.yml"),
             GetPackageSharePath("art_localization_launch", "config", "EKF_param.yml"),
