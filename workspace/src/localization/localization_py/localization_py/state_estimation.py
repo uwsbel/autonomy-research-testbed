95090b137c17548fd3ae309ef034c599ced96fc4
StefanCaldararu <52421529+StefanCaldararu@users.noreply.github.com>
2023-10-10 09:10:50 -0500

    state estimation update (#30)
    * initial file checkout
    
    * edit launch for new launch descriptions
    
    * add scipy
    
    * fix Aaron comments; add docstrings, yaml parameter file, filenames
    
    * add choices for estimation_mode parameter; update docstrings
    
    * Update workspace/src/common/launch/art_localization_launch/launch/art_localization.launch.py
    
    Co-authored-by: Aaron Young <43622931+AaronYoung5@users.noreply.github.com>
    
    * small naming fixes
    
    * refactor dynamics, add paths as arguments
    
    * fix multiple publishers bug again; Now seems to work, but /vehicle_state still has 2 publishers (can't find second one in rqt...
    
    * hard-coded paths removal
    
    * remove unneeded arg
    
    * Removed old param files
    
    * Fixed small bugs regarding the localization config files
    
    * fix Huzaifa's comments
    
    * fix dt gps for Harry <3
    
    * Update state_estimation.py - comment quats
    
    * fix linebreaks, remove unneeded subs
    
    * edit paths for include files, enum params
    
    ---------
    
    Co-authored-by: Aaron Young <43622931+AaronYoung5@users.noreply.github.com>
    Co-authored-by: AaronYoung5 <aryoung@mit.edu>

--- workspace/src/localization/localization_py/localization_py/state_estimation.py
+++ workspace/src/localization/localization_py/localization_py/state_estimation.py
@@ -1,190 +1,243 @@
+import csv
 import rclpy
 from rclpy.node import Node
 from art_msgs.msg import VehicleState
-#from nav_msgs.msg import 
 from sensor_msgs.msg import Imu, NavSatFix, MagneticField
-#from sensor_msgs.msg import NavSatFix
+from chrono_ros_msgs.msg import ChVehicle
 from ament_index_python.packages import get_package_share_directory
-
 import matplotlib.pyplot as plt
 import matplotlib
 import math
 import numpy as np
-
-
 import sys
 import os
+from enum import Enum
+from localization_py.EKF import EKF
+from localization_py.particle_filter import ParticleFilter as PF
+from localization_py.chrono_coordinate_transfer import Graph
+
+class EstimationAlgorithmOption(Enum):
+    GROUND_TRUTH = "ground_truth"
+    EXTENDED_KALMAN_FILTER = "extended_kalman_filter"
+    PARTICLE_FILTER = "particle_filter"
 
 
-#TODO: what does this do?
-ament_tools_root = os.path.join(os.path.dirname(__file__), '.')
-sys.path.insert(0, os.path.abspath(ament_tools_root))
-
-from KalmanFilter import KalmanFilter
-from Chrono_coordinate_transfer import graph
 class StateEstimationNode(Node):
     def __init__(self):
         super().__init__('state_estimation_node')
         
-        self.declare_parameter('vis', False)
-        self.vis = self.get_parameter('vis').get_parameter_value().bool_value
+        # ROS PARAMETERS
+        self.use_sim_msg = self.get_parameter("use_sim_time").get_parameter_value().bool_value
+
+        self.declare_parameter('estimation_alg', EstimationAlgorithmOption.GROUND_TRUTH)
+        self.estimation_alg = self.get_parameter('estimation_alg').get_parameter_value().string_value
+
+        #EKF parameters
+        self.declare_parameter('Q1', 0.1)
+        Q1 = self.get_parameter('Q1').get_parameter_value().double_value
+        self.declare_parameter('Q3', 3)
+        Q3 = self.get_parameter('Q3').get_parameter_value().double_value
+        self.declare_parameter('Q4', 0.1)
+        Q4 = self.get_parameter('Q4').get_parameter_value().double_value
+        self.declare_parameter('R1', 0.0)
+        R1 = self.get_parameter('R1').get_parameter_value().double_value
+        self.declare_parameter('R3', 0.3)
+        R3 = self.get_parameter('R3').get_parameter_value().double_value
+        Q = [Q1, Q1, Q3, Q4]
+        R = [R1, R1, R3]
+
+        #dynamics parameters
+        self.declare_parameter('c_1', 0.0001)
+        c_1 = self.get_parameter('c_1').get_parameter_value().double_value
+        self.declare_parameter('c_0', 0.02)
+        c_0 = self.get_parameter('c_0').get_parameter_value().double_value
+        self.declare_parameter('l', 0.5)
+        l = self.get_parameter('l').get_parameter_value().double_value
+        self.declare_parameter('r_wheel', 0.08451952624)
+        r_wheel = self.get_parameter('r_wheel').get_parameter_value().double_value
+        self.declare_parameter('i_wheel', 0.001)
+        i_wheel = self.get_parameter('i_wheel').get_parameter_value().double_value
+        self.declare_parameter('gamma', 0.33333333)
+        gamma = self.get_parameter('gamma').get_parameter_value().double_value
+        self.declare_parameter('tau_0', 0.3)
+        tau_0 = self.get_parameter('tau_0').get_parameter_value().double_value
+        self.declare_parameter('omega_0', 30.0)
+        omega_0 = self.get_parameter('omega_0').get_parameter_value().double_value
+        dyn = [c_1, c_0, l, r_wheel, i_wheel, gamma, tau_0, omega_0]
+        self.get_logger().info(str(dyn))
 
         #update frequency of this node
         self.freq = 10.0
 
-        if(self.vis):
-            matplotlib.use("TKAgg")
-            #data that will be used by this class
-            #TODO: a lot of these variables aren't used, this data needs to be cleaned up
-            self.fig = plt.figure()
-            self.fig.suptitle('Kalman Filter', fontsize = 20)
         self.gps = ""
-        self.groundTruth = ""
-        #self.imu = ""
+        self.ground_truth = ""
         self.mag = ""
-        self.state = ""
+        # NOTE: Remove this when we transition to CHRONO::ROS. This is bad practice, but the best solution for now
+        if self.use_sim_msg:
+            global VehicleInput
+            from chrono_ros_msgs.msg import ChDriverInputs as VehicleInput
+        else:
+            global VehicleInput
+            from art_msgs.msg import VehicleInput
 
-        #mag measurements
-        self.mag_x = 0
-        self.mag_y = 0
-        self.mag_z = 0
-        #lat, lon, ground truth of each
-        self.lat = 0
-        self.lon = 0
-        self.gt_lat = 0
-        self.gt_lon = 0
-
-        #x, y, kf predictions of each, history of each
+        #x, y, from measurements
         self.x = 0
         self.y = 0
-        self.kfx = 0
-        self.kfy = 0
-        self.hkfx = []
-        self.hkfy = []
-        self.hx = []
-        self.hy = []
-
-        #true X, True Y, History of each
-        self.ty = 0
-        self.tx = 0
-        self.htx = []
-        self.hty = []
-
-        #origin, and whether or not the origin has been set yet.
-        self.origin = [0,0]
-        self.origin_set = False
-
-        #IMU data, x, y, z acceleration
-        self.x_accel = 0
-        self.y_accel = 0
-        self.z_accel = 0
-
-
-        #time between imu updates, sec
-        self.dt_imu = .1
-
-        self.accumulated_error = 0
+        
+        #what we will be using for our state vector. (x, y, theta yaw, v vel)
+        self.state = np.zeros((4,1))
+        np.vstack(self.state)
 
         
-        sim = True
+        self.init_x = 0.0
+        self.init_y = 0.0
+        self.init_theta = 0.0
+        self.init_v = 0.0
+        self.state[0,0] = self.init_x
+        self.state[1,0] = self.init_y
+        self.state[2,0] = self.init_theta
+        self.state[3,0] = self.init_v
 
-        #our graph object
-        self.graph =  graph()
-        #TODO: Still need to do some parameter fitting for this
-        #Kalman Filter for x and y directions, and multiplier for their data.
-        self.kf_x = KalmanFilter(0.2, 0.1, 1.2, 1)
-        self.kf_y = KalmanFilter(0.2, 0.1, 1.2, 1)
-        self.kf_data_multiplier = 10
+        self.gps_ready = False
+
+        #true X, True Y, velocity, z, heading(degrees)
+        self.gtvy = 0
+        self.gty = 0
+        self.gtvx = 0
+        self.gtx = 0
+        self.gtz = 0
+        self.D = 0
+
+        #origin, and whether or not the origin has been set yet.
+        self.origin_set = False
+        self.orig_heading_set = False
+
+
+        #inputs to the vehicle
+        self.throttle = 0.0
+        self.steering = 0
+
+        #time between imu updates, sec
+        self.dt_gps = 1/self.freq
+        
+        #filter
+        if(self.estimation_alg == EstimationAlgorithmOption.EXTENDED_KALMAN_FILTER):
+            self.ekf = EKF(self.dt_gps, dyn, Q, R)
+        elif(self.estimation_alg == EstimationAlgorithmOption.PARTICLE_FILTER):
+            self.pf = PF(self.dt_gps, dyn)
+
+        #our graph object, for reference frame
+        self.graph = Graph()
 
         #subscribers
         self.sub_gps = self.create_subscription(NavSatFix, '~/input/gps', self.gps_callback, 1)
-        if(sim):
-            self.sub_groundTruth = self.create_subscription(NavSatFix, '~/input/groundTruth', self.groundTruth_callback, 1)
-        self.sub_mag = self.create_subscription(MagneticField, "~/input/magnetometer", self.mag_callback, 10)
-        self.sub_gyro = self.create_subscription(Imu, "~/input/gyroscope", self.gyro_callback, 10)
-        self.sub_accel = self.create_subscription(Imu, "~/input/accelerometer", self.accel_callback, 10)
+        if(self.use_sim_msg):
+            self.sub_groud_truth = self.create_subscription(ChVehicle, '~/input/ground_truth', self.ground_truth_callback, 1)
 
+        self.sub_mag = self.create_subscription(MagneticField, "~/input/magnetometer", self.mag_callback, 1)
+        self.sub_control = self.create_subscription(VehicleInput, "~/input/vehicle_inputs", self.inputs_callback, 1)
         #publishers
-        #TODO: change callback time.
-        self.pub_objects = self.create_publisher(VehicleState, '~/output/vehicle_state', 1)
+        self.pub_objects = self.create_publisher(VehicleState, '~/output/vehicle/filtered_state', 1)
         self.timer = self.create_timer(1/self.freq, self.pub_callback)
-    #CALLBACKS:
-    def accel_callback(self, msg):
-        self.accel = msg
-    def gyro_callback(self, msg):
-        self.gyro = msg
-    def groundTruth_callback(self,msg):
-        self.groundTruth = msg
 
+    #CALLBACKS:
+    def inputs_callback(self, msg):
+        self.inputs = msg
+        self.steering = self.inputs.steering
+        self.throttle = self.inputs.throttle
+
+    def ground_truth_callback(self, msg):
+        self.gtx = msg.pose.position.x
+        self.gty = msg.pose.position.y
+        self.gtvx = msg.twist.linear.x
+        self.gtvy = msg.twist.linear.y
+        
     def mag_callback(self,msg):
         self.mag = msg
-        self.mag_x = self.mag.magnetic_field.x
-        self.mag_y = self.mag.magnetic_field.y
-        self.mag_z = self.mag.magnetic_field.z
-        #TODO: is the 0.4 needed??
-        xGauss = self.mag_x*0.48828125
-        yGauss = self.mag_y*0.4882815
+        mag_x = self.mag.magnetic_field.x
+        mag_y = self.mag.magnetic_field.y
+        mag_z = self.mag.magnetic_field.z
+        xGauss = mag_x*0.48828125
+        yGauss = mag_y*0.4882815
         if(xGauss==0):
             if(yGauss<0):
-                D = 0
+                self.D = 0
             else:
-                D = 90
+                self.D = 90
         else:
-            D = math.atan2(yGauss,xGauss)*180/math.pi
-        while(D>360):
-            D = D-360
-        while(D<0):
-            D = D+360
+            self.D = math.atan2(yGauss,xGauss)*180/math.pi
+        while(self.D>360):
+            self.D = self.D-360
+        while(self.D<0):
+            self.D = self.D+360
+        
+        if(not self.orig_heading_set):
+            self.orig_heading_set = True
+            self.graph.set_rotation(np.deg2rad(self.D) -self.init_theta)
+            self.state[2,0] = self.init_theta
 
     def gps_callback(self,msg):
         self.gps = msg
-        self.lat = self.gps.latitude
-        self.lon = self.gps.longitude
-        self.alt = self.gps.altitude
+        self.gps_ready = True
+        if(math.isnan(self.gps.latitude)):
+            #arbitrary values for when we don't get any data (in reality)
+            self.lat = -10
+            self.lon = -10
+            self.alt = -10
+        else:
+            self.lat = self.gps.latitude
+            self.lon = self.gps.longitude
+            self.alt = self.gps.altitude
+
         if(not self.origin_set):
-            self.origin_set = True
-            self.graph.set_graph(self.lat,self.lon, self.alt)
-        self.x,self.y, self.z =self.graph.gps2cartesian(self.lat,self.lon,self.alt)
-        self.hx.append(self.x)
-        self.hy.append(self.y)
-
-
-
-    
-    #callback to run a loop and publish data this class generates
-    def pub_callback(self):
-        self.KFstep()
+           self.origin_set = True
+           self.graph.set_graph(self.lat,self.lon, self.alt)
         
-        self.hkfx.append(self.kfx)
-        self.hkfy.append(self.kfy)
-        if(self.vis):
-            plt.cla()
-            plt.plot(self.hkfx, self.hkfy, label = 'KF predictions', color = 'b', linewidth = 0.5)
-        #   plt.plot(self.htx, self.hty, label = 'True Position', color = 'g', linewidth = 0.5)
-            plt.plot(self.hx, self.hy, label='Measured Position', color = 'r')
-            plt.xlabel('Position_x (m)', fontsize=20)
-            plt.ylabel('Position_y (m)', fontsize=20)
-            plt.legend()
-            plt.draw()
-            plt.pause(0.0001)
-        error = math.sqrt((self.x)**2 + self.y**2) - math.sqrt(self.kfx**2+self.kfy**2)
-        self.accumulated_error = self.accumulated_error + abs(error)
+        x,y,z = self.graph.gps2cartesian(self.lat,self.lon,self.alt)
+        if(self.orig_heading_set):
+            self.x,self.y, self.z =self.graph.rotate(x,y,z)
+            self.x +=self.init_x
+            self.y +=self.init_y
 
+     #callback to run a loop and publish data this class generates
+    def pub_callback(self):
+        u = np.array([[self.throttle], [self.steering/2.2]])
 
+        z = np.array([[self.x],[self.y], [np.deg2rad(self.D)]])
+                
+        if(self.estimation_alg == EstimationAlgorithmOption.EXTENDED_KALMAN_FILTER):
+            self.EKFstep(u, z)
+        elif(self.estimation_alg == EstimationAlgorithmOption.PARTICLE_FILTER):
+            self.PFstep(u,z)
+    
         msg = VehicleState()
         #pos and velocity are in meters, from the origin, [x, y, z]
-        #TODO: is this right?
-        msg.pose.position.x = float(self.kfx)
-        msg.pose.position.y = float(self.kfy)
-
-        # add header timestamps
+        if(self.estimation_alg == EstimationAlgorithmOption.GROUND_TRUTH):
+            msg.pose.position.x = float(self.gtx)
+            msg.pose.position.y = float(self.gty)
+            #TODO: this should be a quat in the future, not the heading.
+            msg.pose.orientation.z = np.deg2rad(self.D)
+            msg.twist.linear.x = float(self.gtvx)
+            msg.twist.linear.y = float(self.gtvy)
+        else:
+            msg.pose.position.x = float(self.state[0,0])
+            msg.pose.position.y = float(self.state[1,0])
+            msg.pose.orientation.z = float(self.state[2,0])
+            msg.twist.linear.x = float(self.state[3,0]*math.cos(self.state[2,0]))
+            msg.twist.linear.y = float(self.state[3,0]*math.sin(self.state[2,0]))
+        
         msg.header.stamp = self.get_clock().now().to_msg()
         self.pub_objects.publish(msg)
-    def KFstep(self):
-        self.kf_x.update(self.x*self.kf_data_multiplier)
-        self.kf_y.update(self.y*self.kf_data_multiplier)
-        self.kfx = self.kf_x.predict()[0]/self.kf_data_multiplier
-        self.kfy = self.kf_y.predict()[0]/self.kf_data_multiplier
+    
+    def EKFstep(self, u, z):
+        self.state = self.ekf.predict(self.state, u)
+        if(self.gps_ready):
+            self.state = self.ekf.correct(self.state, z)
+            self.gps_ready = False
+
+    def PFstep(self, u, z):
+        self.state = self.pf.update(u, z)
+
 def main(args=None):
     print("=== Starting State Estimation Node ===")
     rclpy.init(args=args)
