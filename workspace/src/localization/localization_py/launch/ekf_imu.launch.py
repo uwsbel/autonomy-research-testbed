1de52e7dbc25b8c24491e906f4f65a06e8dacd32
Ishaan Mahajan <ishaanamahajan@gmail.com>
2023-11-03 18:28:33 -0500

    adding

---
+++ workspace/src/localization/localization_py/launch/ekf_imu.launch.py
@@ -0,0 +1,14 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_filter_node',
+            output='screen',
+            parameters=[{'../ekf_config.yaml'}]
+        ),
+    ])
+
