53ab6942fbdbe9dce28effedad83029b189bf300
Aaron Young <43622931+AaronYoung5@users.noreply.github.com>
2023-11-09 20:06:02 -0500

    Vehicle specific services (#60)
    * Updated for metapackages
    
    * Updated based on stefan's comments.
    
    * Updated for metapackages
    
    * Added vehicle folder with script and bashrc profile. Also udpated atk.env/atk.yml to support different vehicle options
    
    * Updated scripts and profile
    
    * Updated setup script
    
    * Added art5
    
    * Added args to dockerfiles docs
    
    * Added art5 metapackage
    
    * updated setup script
    
    Fixed merge
    
    Fixed merge
    
    * Update setup.sh
    
    * Update setup.sh

--- docker/snippets/rosdep.dockerfile
+++ docker/snippets/rosdep.dockerfile
@@ -5,15 +5,16 @@
 # Install rosdep
 RUN apt-get update && \
       apt-get install -y python3-rosdep && \
-      rosdep init && \
+      (if [ ! -f /etc/ros/rosdep/sources.list.d/20-default.list ]; then rosdep init; fi) && \
       rosdep update && \
       apt-get clean && apt-get autoremove -y && rm -rf /var/lib/apt/lists/*
 
 ARG ROS_DISTRO
 ARG ROS_WORKSPACE="./workspace"
 ARG ROSDEP_METAPACKAGE=""
+ARG ROS_INSTALL_PREFIX="/opt/ros/${ROS_DISTRO}/"
 COPY ${ROS_WORKSPACE}/src /tmp/workspace/src
-RUN . /opt/ros/${ROS_DISTRO}/setup.sh && \
+RUN . ${ROS_INSTALL_PREFIX}/setup.sh && \
     cd /tmp/workspace && \
     apt-get update && \
     if [ -z "$ROSDEP_METAPACKAGE" ]; then \
---
+++ vehicles/setup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+# This script setups vehicle computers
+
+if [[ -n $SUDO_USER ]]; then
+    echo "This script should not be run as root"
+    exit 1
+fi
+
+echo "WARNING: You may be asked for the sudo password."
+sudo -v
+
+# =====
+# Setup
+
+mkdir -p ~/sbel
+
+touch ~/.bashrc
+echo "" >> ~/.bashrc
+echo "# Custom config for sbel vehicles. DON'T CHANGE!!!" >> ~/.bashrc
+
+# ============
+# Jetson modes
+
+if [ -f /etc/nv_tegra_release ]; then
+  echo "Setting up Tegra device."
+
+  sudo /usr/sbin/nvpmodel -m 0 # Max power
+
+  # Set fan power mode to cool
+  sudo sed -i '/FAN_DEFAULT_PROFILE/s/quiet/cool/' /etc/nvfancontrol.conf
+  sudo systemctl stop nvfancontrol
+  sudo rm /var/lib/nvfancontrol/status
+  sudo systemctl start nvfancontrol
+fi
+
+# ======
+# Apt
+
+echo "Updating apt."
+
+sudo apt update
+sudo apt install -y tmux
+sudo apt autoremove
+
+# ======
+# Docker
+
+echo "Setting up Docker."
+
+# Docker compose
+sudo apt-get install ca-certificates curl gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt update
+sudo apt install docker-compose-plugin -y
+
+# Add nvidia as the default runtime
+if [ -f /etc/nv_tegra_release ]; then
+  grep -q '"default-runtime": "nvidia"' /etc/docker/daemon.json || sudo sed -i '/"data-root"/a \    "default-runtime": "nvidia",' /etc/docker/daemon.json
+fi
+
+# Create the docker group
+sudo groupadd docker 2>/dev/null
+# Add the user to the docker group
+sudo usermod -aG docker $USER
+
+# =========
+# Miniconda
+
+echo "Installing up miniconda."
+
+# Install
+mkdir -p ~/.conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh -O ~/.conda/miniconda.sh
+bash ~/.conda/miniconda.sh -b -u -p ~/.conda
+rm -rf ~/.conda/miniconda.sh
+~/.conda/bin/conda init bash
+
+# Create the conda environment
+~/.conda/bin/conda create -p ~/.conda/envs/sbel python=3.12 --yes
+
+# ==============
+# Clone the repo
+
+echo "Cloning autonomy-research-testbed."
+
+git clone https://github.com/uwsbel/autonomy-research-testbed.git ~/sbel/autonomy-research-testbed
+
+echo "source ~/sbel/autonomy-research-testbed/vehicles/bashrc" >> ~/.bashrc
+
+~/.conda/bin/conda run --live-stream -p ~/.conda/envs/sbel pip install -r ~/sbel/autonomy-research-testbed/requirements.txt
+(cd ~/sbel/autonomy-research-testbed && ~/.conda/bin/conda run --live-stream -p ~/.conda/envs/sbel pre-commit install)
+(cd ~/sbel/autonomy-research-testbed && git remote set-url origin git@github.com:uwsbel/autonomy-research-testbed.git)
+
+echo "Done! You should logout and log back in to activate changes."
