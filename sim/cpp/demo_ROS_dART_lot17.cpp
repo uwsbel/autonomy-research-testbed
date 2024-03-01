7a871182d758aee6b04a55b68e80a6c229c66b43
Ishaan <ishaanamahajan@gmail.com>
2024-03-01 14:55:07 -0600

    adding all 6 gps models

--- sim/cpp/demo_ROS_dART_lot17.cpp
+++ sim/cpp/demo_ROS_dART_lot17.cpp
@@ -24,7 +24,7 @@
 #include "chrono/core/ChTypes.h"
 
 #include "chrono/physics/ChSystemNSC.h"
-#include "chrono/assets/ChVisualShapeTriangleMesh.h"
+//#include "chrono/assets/ChVisualShapeTriangleMesh.h"
 #include "chrono/assets/ChVisualMaterial.h"
 #include "chrono/assets/ChVisualShape.h"
 #include "chrono/geometry/ChTriangleMeshConnected.h"
@@ -76,6 +76,7 @@
 #include "chrono_ros/handlers/sensor/ChROSMagnetometerHandler.h"
 #include "chrono_ros/handlers/sensor/ChROSLidarHandler.h"
 #include "chrono_ros/handlers/sensor/ChROSGPSHandler.h"
+#include "chrono_ros/handlers/sensor/ChROSAirSimGPSHandler.h"
 
 #include <chrono>
 #include <random>
@@ -86,7 +87,7 @@ using namespace chrono::sensor;
 //using namespace chrono::vehicle::artvehicle;
 // =============================================================================
 // Initial vehicle location and orientation
-ChVector<> initLoc(-1.0, -1.0, 0.5);
+ChVector<> initLoc(0.0, 0.0, 0.5);
 ChVector<> initLoc_flw(0, 0.0, 0.5);
 ChQuaternion<> initRot = Q_from_AngZ(0.0f);
 // Rigid terrain
@@ -137,8 +138,8 @@ int main(int argc, char* argv[]) {
     vehicle.SetInitPosition(ChCoordsys<>(initLoc, initRot));
     vehicle.SetTireType(tire_model);
     vehicle.SetTireStepSize(step_size);
-    vehicle.SetMaxMotorVoltageRatio(0.09f);
-    vehicle.SetStallTorque(0.3f);
+    vehicle.SetMaxMotorVoltageRatio(0.18f);
+    vehicle.SetStallTorque(0.5f);
     vehicle.SetTireRollingResistance(0.05f);
     vehicle.Initialize();
 
@@ -155,6 +156,15 @@ int main(int argc, char* argv[]) {
     // Containing system
     auto system = vehicle.GetSystem();
 
+    system->SetTimestepperType(ChTimestepper::Type::EULER_IMPLICIT);
+    // system->SetTimestepperType(ChTimestepper::Type::HHT);
+    // auto integrator = std::static_pointer_cast<ChTimestepperHHT>(system->GetTimestepper());
+    // integrator->SetAlpha(-0.2);
+    // integrator->SetMaxiters(8);
+    // integrator->SetAbsTolerances(1e-1, 10);
+    // integrator->SetModifiedNewton(true);
+    // integrator->SetVerbose(true);
+
     // Add box in front of the vehicle
     auto vis_mat = chrono_types::make_shared<ChVisualMaterial>();
     vis_mat->SetAmbientColor({0.f, 0.f, 0.f});
@@ -169,7 +179,7 @@ int main(int argc, char* argv[]) {
     // Function to read and parse the CSV file
     std::vector<std::tuple<double, double, double, double>> positions;
     // std::string directoryPath = "/home/art/art/sim/data/autonomy-toolkit/paths/lot17_sinsquare.csv";
-    std::string csvFile =  "/home/art/art/sim/data/autonomy-toolkit/paths/lot17_sinsquare.csv"; // Replace with CSV file path
+    std::string csvFile =  "/home/art/art/sim/data/autonomy-toolkit/paths/path.csv"; // Replace with CSV file path
     std::ifstream file(csvFile);
     if (!file.is_open()) {
         std::cerr << "Error: Unable to open CSV file." << std::endl;
@@ -245,12 +255,12 @@ int main(int argc, char* argv[]) {
     cam->SetName("Camera");
     cam->SetLag(lag);
     cam->SetCollectionWindow(0.0f);
-    //cam->PushFilter(chrono_types::make_shared<ChFilterVisualize>(image_width, image_height, "Camera"));
+    cam->PushFilter(chrono_types::make_shared<ChFilterVisualize>(image_width, image_height, "Camera"));
     std::cout << "Initialization completed 4." << std::endl;
     //cam->PushFilter(chrono_types::make_shared<ChFilterSave>("./cam1/"));
     
-    // manager->AddSensor(cam);
-    // std::cout << "Initialization completed 5." << std::endl;
+    manager->AddSensor(cam);
+    std::cout << "Initialization completed 5." << std::endl;
     // manager->Update();
     //------------
 
@@ -261,24 +271,38 @@ int main(int argc, char* argv[]) {
     // manager->AddSensor(cam);
     
 
-    auto sensor_offset_pose = chrono::ChFrame<double>({0, 0, 0}, Q_from_AngAxis(0, {0, 0, 0}));
+    auto imu_offset_pose = chrono::ChFrame<double>({0, 0, 0}, Q_from_AngAxis(CH_C_PI, {1, 0, 0}));
+    auto sensor_offset_pose = chrono::ChFrame<double>({0, 0, 0}, Q_from_AngAxis(0, {1, 0, 0}));
+    //auto sensor_offset_pose = chrono::ChFrame<double>({0, 0, 0}, Q_from_AngAxis(CH_C_PI, {1, 0, 0}));
     auto noise_none = chrono_types::make_shared<ChNoiseNone>();
     ChVector<> gps_reference(-89.400, 43.070, 260.0);
 
     ChVector<double> gyro_noise_mean(0, 0, 0);
     ChVector<double> gyro_noise_stdev(0.05, 0.05, 0.05); // degrees per second
     ChVector<double> accel_noise_mean(0.0, 0.0, 0.0);
-    ChVector<double> accel_noise_stdev(0.01, 0.01, 0.01); // m/s²
+    ChVector<double> accel_noise_stdev(0.30, 0.30, 0.01); // m/s²
+    std::cout << "Initialization completed 5.1." << std::endl;
 
 
 
-    auto gyro_noise_model = chrono_types::make_shared<ChNoiseNormal>(
-        gyro_noise_mean, gyro_noise_stdev);
-    auto accel_noise_model = chrono_types::make_shared<ChNoiseNormal>(
-        accel_noise_mean, accel_noise_stdev);
+    auto gyro_noise_model = chrono_types::make_shared<ChNoiseNormalDrift>(100,
+        gyro_noise_mean, gyro_noise_stdev, 0.0001, 0.1);
+    auto accel_noise_model = chrono_types::make_shared<ChNoiseNormalDrift>(100,
+        accel_noise_mean, accel_noise_stdev, 0.1, 0.1);
+
+    // auto gyro_noise_model = chrono_types::make_shared<ChNoiseNormal>(
+    //     gyro_noise_mean, gyro_noise_stdev);
+    // auto accel_noise_model = chrono_types::make_shared<ChNoiseNormal>(
+    //     accel_noise_mean, accel_noise_stdev);
+
+    auto mag_noise_model = chrono_types::make_shared<ChNoiseNormal>(ChVector<double>({0., 0., 0.}),            // float mean,
+                                                         ChVector<double>({0.001, 0.001, 0.001}));
 
 
     ChFrame<double> sensor_frame(ChVector<>(0, 0, 0), ChQuaternion<>(1, 0, 0, 0));
+
+   
+
 
 
     // Accelerometer
@@ -287,22 +311,44 @@ int main(int argc, char* argv[]) {
     manager->AddSensor(accelerometer);
 
     // Gyroscope
-    auto gyroscope = chrono_types::make_shared<ChGyroscopeSensor>(vehicle.GetChassisBody(), 100, sensor_offset_pose,gyro_noise_model);
+    auto gyroscope = chrono_types::make_shared<ChGyroscopeSensor>(vehicle.GetChassisBody(), 100, sensor_offset_pose, noise_none);
     gyroscope->PushFilter(chrono_types::make_shared<ChFilterGyroAccess>());
     manager->AddSensor(gyroscope);
 
     // Magnetometer
-    auto magnetometer = chrono_types::make_shared<ChMagnetometerSensor>(vehicle.GetChassisBody(), 100, sensor_offset_pose, noise_none, gps_reference);
+    auto magnetometer = chrono_types::make_shared<ChMagnetometerSensor>(vehicle.GetChassisBody(), 100, sensor_offset_pose, mag_noise_model, gps_reference);
     magnetometer->PushFilter(chrono_types::make_shared<ChFilterMagnetAccess>());
     manager->AddSensor(magnetometer);
 
     ChVector<double> gps_noise_mean(0, 0, 0);
-    ChVector<double> gps_noise_stdev(0.017, 0.017, 0.017); // meters
+    ChVector<double> gps_noise_stdev(0.03, 0.03, 0.03); // meters
 
     auto gps_noise_model = chrono_types::make_shared<ChNoiseNormal>(
     gps_noise_mean, gps_noise_stdev);
 
+    ChVector<> gps_reference1(-89.412240758, 43.071986683, 0);
+
+
+   
+    std::cout << "Initialization completed 5.2" << std::endl;
+
+    auto random_walk_noise =
+        chrono_types::make_shared<ChNoiseRandomWalks>(0,                             // mean
+                                                      0.047,                         // std
+                                                      100,  // Model Update rate
+                                                      0.016,                         // max velocity
+                                                      0.152,                         // max acc
+                                                      gps_reference1);
+
     auto gps = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, noise_none);
+    //auto gps = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, random_walk_noise);
+    // auto gps1 = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, noise_none);
+    // auto gps2 = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, gps_noise_model);
+    // auto gps3 = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, random_walk_noise);
+    
+
+
+    
     gps->PushFilter(chrono_types::make_shared<ChFilterGPSAccess>());
     manager->AddSensor(gps);
     manager->Update();
@@ -314,6 +360,8 @@ int main(int argc, char* argv[]) {
     // The clock automatically publishes on every tick and on topic /clock
     auto clock_handler = chrono_types::make_shared<ChROSClockHandler>();
     ros_manager->RegisterHandler(clock_handler);
+
+    std::cout << "Initialization completed 5.3" << std::endl;
 
 
     // Create a subscriber to the driver inputs
@@ -335,14 +383,80 @@ int main(int argc, char* argv[]) {
     auto magnetometer_handler = chrono_types::make_shared<ChROSMagnetometerHandler>(magnetometer, magnetometer_topic_name);
     ros_manager->RegisterHandler(magnetometer_handler);
 
+    
+    // take command line input for hdop
+    //float hdop = std::stof(argv[1]);
 
     auto gps_topic_name = "~/output/gps/data";
+    
+    //auto gps_handler = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps, gps_topic_name, 100, 100,0.85,  0.1, 12.4);
+    //auto gps_handler = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps, gps_topic_name, 100, 100, 0.85,  0.1, 12);
+    
     auto gps_handler = chrono_types::make_shared<ChROSGPSHandler>(gps, gps_topic_name);
     ros_manager->RegisterHandler(gps_handler);
 
+    auto gps1 = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, noise_none);
+    auto gps2 = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, gps_noise_model);
+    auto gps3 = chrono_types::make_shared<ChGPSSensor>(vehicle.GetChassis()->GetBody(), 10.f, sensor_offset_pose, gps_reference, random_walk_noise);
+
+    gps1->PushFilter(chrono_types::make_shared<ChFilterGPSAccess>());
+    manager->AddSensor(gps1);
+    manager->Update();
+
+    gps2->PushFilter(chrono_types::make_shared<ChFilterGPSAccess>());
+    manager->AddSensor(gps2);
+    manager->Update();
+
+    gps3->PushFilter(chrono_types::make_shared<ChFilterGPSAccess>());
+    manager->AddSensor(gps3);
+    manager->Update();
+
+
+    // Create ROS handler for GPS 1
+    auto gps1_topic_name = "~/output/gps/gps1";
+    auto gps1_ros_handler = chrono_types::make_shared<ChROSGPSHandler>(gps1, gps1_topic_name);
+    ros_manager->RegisterHandler(gps1_ros_handler);
+
+    // Create AirSim handler for GPS 1
+    auto gps1_airsim_topic_name = "~/output/gps/gps1_airsim";
+    auto gps1_airsim_handler = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps1, gps1_airsim_topic_name, 100, 100, 0.85, 0.1, 12.4);
+    ros_manager->RegisterHandler(gps1_airsim_handler);
+
+    auto gps1_airsim_default_topic_name = "~/output/gps/gps1_airsim_default";
+    auto gps1_airsim_handler_default = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps1, gps1_airsim_topic_name, 100, 100, 0.1, 0.1, 0.9);
+    ros_manager->RegisterHandler(gps1_airsim_handler_default);
+
+    // Create ROS handler for GPS 2
+    auto gps2_topic_name = "~/output/gps/gps2";
+    auto gps2_ros_handler = chrono_types::make_shared<ChROSGPSHandler>(gps2, gps2_topic_name);
+    ros_manager->RegisterHandler(gps2_ros_handler);
+
+    // Create AirSim handler for GPS 2
+    auto gps2_airsim_topic_name = "~/output/gps/gps2_airsim";
+    auto gps2_airsim_handler = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps2, gps2_airsim_topic_name, 100, 100, 0.85, 0.1, 12.4);
+    ros_manager->RegisterHandler(gps2_airsim_handler);
+
+    auto gps2_airsim_default_topic_name = "~/output/gps/gps2_airsim_default";
+    auto gps2_airsim_handler_default = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps2, gps2_airsim_topic_name, 100, 100, 0.1, 0.1, 0.9);
+    ros_manager->RegisterHandler(gps2_airsim_handler_default);
+
+    // Create ROS handler for GPS 3
+    auto gps3_topic_name = "~/output/gps/gps3";
+    auto gps3_ros_handler = chrono_types::make_shared<ChROSGPSHandler>(gps3, gps3_topic_name);
+    ros_manager->RegisterHandler(gps3_ros_handler);
+
+    // Create AirSim handler for GPS 3
+    auto gps3_airsim_topic_name = "~/output/gps/gps3_airsim";
+    auto gps3_airsim_handler = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps3, gps3_airsim_topic_name, 100, 100, 0.85, 0.1, 12.4);
+    ros_manager->RegisterHandler(gps3_airsim_handler);
+
+    auto gps3_airsim_default_topic_name = "~/output/gps/gps3_airsim_default";
+    auto gps3_airsim_handler_default = chrono_types::make_shared<ChROSAirSimGPSHandler>(gps3, gps3_airsim_topic_name, 100, 100, 0.1, 0.1, 0.9);
+    ros_manager->RegisterHandler(gps3_airsim_handler_default);
+
 
     // Create a publisher for the vehicle state
-    auto vehicle_state_rate = 25;
+    auto vehicle_state_rate = 5;
     auto vehicle_state_topic_name = "~/output/vehicle/state";
     auto vehicle_state_handler = chrono_types::make_shared<ChROSBodyHandler>(
         vehicle_state_rate, vehicle.GetChassisBody(), vehicle_state_topic_name);
@@ -353,13 +467,12 @@ int main(int argc, char* argv[]) {
     // Finally, initialize the ros manager
     ros_manager->Initialize();
 
-    double t_end = 300;
+    double t_end = 20;
     double time = 0;
-    double t = 20;
     double interval = 2;
     // Simulation loop
-    vehicle.GetVehicle().EnableRealtime(true);
-    //std::cout << "Initialization completed 3." << std::endl;
+    
+    std::cout << "Initialization completed 6." << std::endl;
 
     while (time < t_end) {
 
@@ -381,12 +494,18 @@ int main(int argc, char* argv[]) {
         vehicle.Advance(step_size);
         terrain.Advance(step_size);
 
+        //std::cout << "Z Accel: " << vehicle.GetSystem()->Get_G_acc() << std::endl;
+        std::cout << "X Acc: " << vehicle.GetChassisBody()->GetPos_dtdt().x() << std::endl;
+    	//std::cout << "Initialization completed 7." << std::endl;
         // update sensor manager
         manager->Update();
 
+   	 //std::cout << "Initialization completed 8." << std::endl;
         if (!ros_manager->Update(time, step_size))
             break;
     }
 
     return 0;
 }
+
+   
