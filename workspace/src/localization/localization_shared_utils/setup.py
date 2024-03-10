2affd5b9d817c635fb8709c6df4315e2854bb012
Ishaan <ishaanamahajan@gmail.com>
2024-03-10 14:28:51 -0500

    adding exec in setup.py

--- workspace/src/localization/localization_shared_utils/setup.py
+++ workspace/src/localization/localization_shared_utils/setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup
+import os
 
 package_name = "localization_shared_utils"
+
+# Automatically include all launch files.
+launch_files = [(os.path.join('share', package_name, 'launch'), 
+                 [os.path.join('launch', file) for file in os.listdir('launch')])]
 
 setup(
     name=package_name,
@@ -9,6 +14,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        *launch_files,  # Add your launch files here
     ],
     install_requires=["setuptools"],
     zip_safe=True,
@@ -20,7 +26,8 @@ setup(
     entry_points={
         "console_scripts": [
             "localization_shared_utils = localization_shared_utils.localization_shared_utils:main",
-            'ins_publisher = localization_shared_utils.ins_publisher:main'
+            'ins_publisher = localization_shared_utils.ins_publisher:main',
+            'ins_gps_publisher = localization_shared_utils.ins_gps_publisher:main'
         ],
     },
 )
