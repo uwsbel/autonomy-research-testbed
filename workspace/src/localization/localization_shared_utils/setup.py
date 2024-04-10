e27c1c4cf20ae52f0698bca4cc6364a19b164a20
Ishaan <ishaanamahajan@gmail.com>
2024-04-10 10:13:01 -0500

    modifying setup.py

--- workspace/src/localization/localization_shared_utils/setup.py
+++ workspace/src/localization/localization_shared_utils/setup.py
@@ -1,25 +1,37 @@
 from setuptools import setup
+import os
 
-package_name = "localization_shared_utils"
+package_name = 'localization_shared_utils'
+
+# It's important to use the full path from the package directory when listing files
+launch_directory = os.path.join('launch')
+launch_files = [
+    os.path.join(launch_directory, file)
+    for file in os.listdir(launch_directory)
+    if file.endswith('.py')  # Make sure this matches your file extensions
+]
 
 setup(
     name=package_name,
-    version="0.0.0",
+    version='0.0.0',
     packages=[package_name],
     data_files=[
-        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
-        ("share/" + package_name, ["package.xml"]),
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), launch_files),
     ],
-    install_requires=["setuptools"],
+    install_requires=['setuptools'],
     zip_safe=True,
-    maintainer="art",
-    maintainer_email="art@todo.todo",
-    description="TODO: Package description",
-    license="TODO: License declaration",
-    tests_require=["pytest"],
+    maintainer='art',
+    maintainer_email='art@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
     entry_points={
-        "console_scripts": [
-            "localization_shared_utils = localization_shared_utils.localization_shared_utils:main"
+        'console_scripts': [
+            'localization_shared_utils = localization_shared_utils.localization_shared_utils:main'
         ],
     },
 )
+
